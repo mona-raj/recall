ba853e5e93b536e2067ac521575a572cd3b5559e
Mona Raj <agrmonaraj@gmail.com>
2026-04-07 10:50:52 +0530

    Refactor ReminderScreen to StatefulWidget and implement time picker functionality

--- lib/screens/reminder_screen.dart
+++ lib/screens/reminder_screen.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
 
-class ReminderScreen extends StatelessWidget {
+class ReminderScreen extends StatefulWidget {
+  @override
+  State<ReminderScreen> createState() => _ReminderScreenState();
+}
+
+class _ReminderScreenState extends State<ReminderScreen> {
+  TimeOfDay? selectedTime;
+
+  Future<void> pickTime(BuildContext context, TimeOfDay? initialTime) async {
+    TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+
+    if (pickedTime != null) {
+      setState(() async {
+        selectedTime = pickedTime;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Reminder Screen"),
+    return Scaffold(
+      appBar: AppBar(title: Text("Add Reminder"), centerTitle: true),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 12,
+            children: [
+              TextField(
+                decoration: InputDecoration(hintText: 'Enter Reminder name'),
+              ),
+              ElevatedButton(
+                onPressed: () => {print("Image picked")},
+                child: Text("Add photo"),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                spacing: 12,
+                children: [
+                  ElevatedButton(
+                    onPressed: () => pickTime(context, selectedTime),
+                    child: Text("Pick time"),
+                  ),
+                  Text("Selected time: ${selectedTime?.format(context)}"),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () => {print("Saved reminder")},
+                child: Text("Save Reminder"),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
